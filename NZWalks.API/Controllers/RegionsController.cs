ce7fd1881f2b8b7457cd9b5f2a0102ad8d382506
Nihat Özhan Keklik <ozhankeklik2001@gmail.com>
2024-04-13 13:54:26 +0300

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:1234/api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //GET ALL REGIONS
+        //GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Get data from database - domain models
+            var regionsDomain = dbContext.Regions.ToList();
+
+            //map domain models to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach(var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Name = regionDomain.Name,
+                    Code = regionDomain.Code,
+                    RegionImageUrl = regionDomain.RegionImageUrl,
+                });
+            }
+
+            //return DTOs
+            return Ok(regionsDto);
+        }
+
+        //GET SINGLE REGION (Get Region By ID)
+        //GET: https://localhost:portnumber/api/regions/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //two possible ways are listed:
+            //var region = dbContext.Regions.Find(id);
+            //Get region domain model from database
+            var regionDomain = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            //map/convert region domain model to region dto
+
+            var regionDto = new RegionDto()
+            {
+                Id = regionDomain.Id,
+                Name = regionDomain.Name,
+                Code = regionDomain.Code,
+                RegionImageUrl = regionDomain.RegionImageUrl,
+            };
+
+            //return dto back to client
+            return Ok(regionDto);
+
+        }
+
+    }
+}
