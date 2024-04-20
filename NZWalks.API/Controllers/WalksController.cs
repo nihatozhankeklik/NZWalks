ff5e3567a4a735ac0d6eaf8c946d4d7bbd4d3a69
Nihat Özhan Keklik <ozhankeklik2001@gmail.com>
2024-04-20 20:45:12 +0300

    Section 5 Finished

---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    // /api/walks
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IWalkRepository walkRepository;
+
+        public WalksController(IMapper mapper, IWalkRepository walkRepository)
+        {
+            this.mapper = mapper;
+            this.walkRepository = walkRepository;
+        }
+
+        //Create walk
+        //post: /api/walks
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddWalkRequestDto addWalkRequestDto)
+        {
+            //Map DTO to Domain Model
+            var walkDomainModel = mapper.Map<Walk>(addWalkRequestDto);
+
+            await walkRepository.CreateAsync(walkDomainModel);
+
+            //Map domain model to dto
+
+            return Ok(mapper.Map<WalkDto>(walkDomainModel));
+
+
+
+
+        }
+
+
+        //Get walks
+        //GET: /api/walks
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var walksDomainModel = await walkRepository.GetAllAsync();
+
+            //Map domain model to dto
+            return Ok(mapper.Map<List<WalkDto>>(walksDomainModel));
+
+        }
+
+        //Get walk by id
+        //GET: /api/walks/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var walkDomainModel = await walkRepository.GetByIdAsync(id);
+
+            if (walkDomainModel == null)
+                return NotFound();
+
+            //Map domain model to dto
+            return Ok(mapper.Map<WalkDto>(walkDomainModel));
+
+        }
+
+        //Update walk by id
+        //PUT: /api/walks/{id}
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute]Guid id, UpdateWalkRequestDto updateWalkRequestDto)
+        {
+            //map dto to domain model
+            var walkDomainModel = mapper.Map<Walk>(updateWalkRequestDto);
+
+            walkDomainModel = await walkRepository.UpdateAsync(id, walkDomainModel);
+
+            if (walkDomainModel == null)
+                return NotFound();
+
+            //map domain model to dto
+            return Ok(mapper.Map<WalkDto>(walkDomainModel));
+
+        }
+
+        //Delete walk by id
+        //DELETE: /api/walks/{id}
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var deletedWalkDomainModel = await walkRepository.DeleteAsync(id);
+            if (deletedWalkDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            //map domain model to dto
+            return Ok(mapper.Map<WalkDto>(deletedWalkDomainModel));
+
+
+
+        }
+
+    }
+}
--- NZWalks.API/Models/Domain/Walk.cs
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -7,8 +7,6 @@
         public string Description { get; set; }
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; }
-
-
         public Guid DifficultyId { get; set; }
         public Guid RegionId { get; set; }
 
